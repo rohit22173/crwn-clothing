972bdcfbc61afd0e98f40458a40a9bfcce8d198c
unknown <rohit22173@gmail.com>
2023-01-12 18:55:54 +0530

    Updated payment to USD

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -7,8 +7,8 @@ exports.handler = async (event) => {
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: 'INR',
-      payment_method_types: ['card']
+      currency: 'usd',
+      payment_method_types: ['card'],
     });
 
     return {
