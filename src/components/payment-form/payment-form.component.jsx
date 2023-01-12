83949256877ebe9b664ff9922de4247b0c270fca
unknown <rohit22173@gmail.com>
2023-01-12 18:52:28 +0530

    Updated the payment currency

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -25,8 +25,7 @@ const PaymentForm = () => {
         const response = await fetch('/.netlify/functions/create-payment-intent', {
             method: 'post',
             headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 amount: amount * 100
